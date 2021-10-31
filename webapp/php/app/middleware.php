4207def8fbb418be9d442e59424b078ff3fca03b
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2021-10-31 18:23:18 +0900

    fix: session storeにcookieを使う

--- webapp/php/app/middleware.php
+++ webapp/php/app/middleware.php
@@ -3,10 +3,19 @@
 declare(strict_types=1);
 
 use Slim\App;
-use Slim\Middleware\Session;
 use App\Application\Middleware\AccessLog;
 
 return function (App $app) {
     $app->add(AccessLog::class);
-    $app->add(Session::class);
+    $app->add(new \Slim\Middleware\SessionCookie([
+        'expires' => '20 minutes',
+        'path' => '/',
+        'domain' => null,
+        'secure' => false,
+        'httponly' => false,
+        'name' => 'slim_session',
+        'secret' => 'tin2',
+        'cipher' => MCRYPT_RIJNDAEL_256,
+        'cipher_mode' => MCRYPT_MODE_CBC
+    ]));
 };
